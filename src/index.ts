519435b37864562489bb667442821d8832b47804
Vitor Vilela <vilelavitor305@gmail.com>
2022-09-24 00:26:04 -0300

    API Finalizada
    A construção foi finalizada com sucesso e tenho certeza que servirá de base para diversos projetos.

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+
+import express from "express";
+import statusRoute from "./routes/status.route";
+import usersRoute from "./routes/users.route";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use(usersRoute);
+app.use(statusRoute);
+
+app.listen(3000, () => {
+    console.log("Aplicação executando na porta 3000");
+}); 
